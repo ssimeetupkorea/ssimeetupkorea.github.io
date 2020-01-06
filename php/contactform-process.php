d7d1572a2e194dd5eb0aaf4081a8cf8f9348655d
Jaehoon Shim <jshim10@illinois.edu>
2020-01-06 19:44:51 +0900

    Update contactform-process.php

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Evolo landing page";
+$EmailTo = "jshim10@illinois.edu";
+$Subject = "New message from SSIMEETUPKOREA";
 
 // prepare email body text
 $Body = "";
